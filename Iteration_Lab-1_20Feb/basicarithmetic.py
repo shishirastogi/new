c406727faab8415d7580bebf2df358ceb126b5ad
shishirastogi <shishirrastogi2@gmail.com>
2026-02-20 10:29:59 +0530

    Day 4

---
+++ Iteration_Lab-1_20Feb/basicarithmetic.py
@@ -0,0 +1,14 @@
+x = int(input("Enter value for number one : "))
+y = int(input("Enter value for number two : "))
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+print(add(x,y))
+print(sub(x,y))
+print(mul(x,y))
+print(int(div(x,y)))
